0a3db0d7e149552074a556935c59f566a48000d5
anusha-developer <83368436+anusha-developer@users.noreply.github.com>
2021-08-29 21:34:31 +0530

    Add files via upload

---
+++ RegistractionModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProjectMVC.Models
+{
+    public class RegistractionModel
+    {
+        public int ProjectId { get; set; }
+        [Required]
+        public string ProjName { get;set;}
+        [Required]
+        public int ProjValue { get;set;}
+        public  DateTime ApplicationDate { get; set; }
+        public string ProjSponser { get; set; }
+        public string ProjStatus { get; set; }
+        public int Targets { get; set; }
+        public DateTime ProjStartDate { get; set; }
+        public DateTime ProjEndDate { get; set; }
+        public int  TotalDays { get; set; }
+        public string ReservationAllowed { get; set; }
+        public string Scheme { get; set; }
+        public string States { get; set; }
+        public string Districts { get; set; }
+        public string Gender { get; set; }
+        public string Caste { get; set; }
+
+
+    }
+}
+
+
---
+++ SampleController.cs
@@ -0,0 +1,67 @@
+﻿using ProjectMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectMVC.Controllers
+{
+    public class SampleController : Controller
+    {
+        // GET: Sample
+
+        static List<RegistractionModel> Registractions = new List<RegistractionModel>();
+        public ActionResult Index()
+        {
+            return View(Registractions);
+        }
+        //  GET:  Sample/Details/1
+        public ActionResult Details(RegistractionModel Registractions)
+        {
+            return View(Registractions);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Create(RegistractionModel reg)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Create", reg);
+            }
+            Registractions.Add(reg);
+            return RedirectToAction("Index");
+        }
+        //GET: Edit
+           /* public ActionResult Edit(Int32 ProjectId)
+        {
+            var register = objContext.TblCompanyTask.Where(x => x.id == ProjectId).FirstOrDefault();
+            if (register != null)
+            {
+                TempData["ProjectId"] = ProjectId;
+                TempData.Keep();
+                return View(register);
+            }
+            return View();
+        }
+        //GET: Delete/1
+        public ActionResult Delete(int ProjectId)
+        {
+            if (ProjectId > 0)
+            {
+                var ProjectId = objContext.TblCompanyTask.Where(x => x.id == ProjectId).FirstOrDefault();
+                if (ProjectId != null)
+                {
+                    objContext.Entry(ProjectId).State = EntityState.Deleted;
+                    objContext.SaveChanges();
+                }
+            }
+            return RedirectToAction("Index");
+        }*/
+
+
+    }
+} 
